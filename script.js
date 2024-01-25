3ef32f0b75201aa8afbeb1f5a2c99d541c384ec2
mrred <coopk2e@gmail.com>
2024-01-25 17:10:54 +1100

    added painting and hover effect

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const container = document.querySelector('.container')
 
-const div = document.createElement('div')
+for (let i = 0; i < 256; i++) {
+    const div = document.createElement('div')
+    container.appendChild(div)
+}
 
+const div = document.querySelector('div')
 
+div.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'gray';
+    event.target.addEventListener('mouseout', (event) => {
+        if (event.srcElement.style.backgroundColor !== 'black') {
+            event.srcElement.removeAttribute('style')
+        }
+    })
+})
 
-container.appendChild(div)
+const paint = (event) => {
+    event.target.classList.add('painted');
+}
+
+div.addEventListener('mousedown', () => {
+    div.addEventListener('mouseover', paint)
+    div.addEventListener('mouseup', () => {
+        div.removeEventListener('mouseover', paint)
+    })
+})
